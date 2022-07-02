cd83ecc72f61b5185ce21cf9cf5d9e5b7ed9976c
SokolovAY <108220161+SokolovAY@users.noreply.github.com>
2022-07-02 17:02:39 +0200

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 import requests
-from snowflake import connector
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -32,9 +31,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', fruit_choice)
